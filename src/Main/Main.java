8c87a9c4ccdc03235850ec9ef0356992d26937b7
Adrian <adrian.suarez.alphen@gmail.com>
2022-12-25 18:21:51 +0100

    Cambios en otra rama

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -10,6 +10,7 @@ public class Main {
 	
 	public static void main(String[] args) {
 		System.out.println("Commit");
+		System.out.println("Commit development");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
