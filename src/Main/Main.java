c023972996fb3a72d6ea80af9a2f9cd049289754
Adrian <adrian.suarez.alphen@gmail.com>
2022-12-25 18:41:19 +0100

    De vuelta a la version Final 2.0

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -9,9 +9,7 @@ public class Main {
 
 	
 	public static void main(String[] args) {
-		System.out.println("Commit");
-		System.out.println("Commit development");
-		System.out.println("Commit development2");
+		
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
