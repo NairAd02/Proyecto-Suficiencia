86ff42f7a5c6055d2da303207e8f3555cf3ca7d6
Adrian <adrian.suarez.alphen@gmail.com>
2022-12-25 18:25:22 +0100

    Segundo cambio en development

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,6 +11,7 @@ public class Main {
 	public static void main(String[] args) {
 		System.out.println("Commit");
 		System.out.println("Commit development");
+		System.out.println("Commit development2");
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
